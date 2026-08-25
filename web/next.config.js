781513d39a259db73ccafb5630e2a360bf8011d9
vallabh2504 <67763007+vallabh2504@users.noreply.github.com>
2026-08-26 03:31:11 +0530

    chore: remove telemetry stack (moved to ESP32_RedundancyTelematics)

--- web/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Required for Vercel edge runtime if you switch to it later
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
